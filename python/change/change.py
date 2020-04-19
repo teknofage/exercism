2139cf028cde4163884cb6ccabb68665182ca05b
teknofage <teknofage@gmail.com>
2020-04-19 00:05:19 -0700

    working but with two functions

--- python/change/change.py
+++ python/change/change.py
@@ -1,21 +1,34 @@
 
-coins = [1, 4, 5]
-target = 12
-def find_fewest_coins(coins, target):
-    pass
+coins = [1, 2, 3, 4, 5]
+k = 7
 
 
-def combinations(coins, k):
-    all_combs = []
-    # for coin in coins: 
-    #     n choose k
+
+
+def combinations(coins, target):
+    valid_list = []
     
-    
-    
-    # print (list(combinations_with_replacement (coins, 3)))
-    # for combination in list(combinations_with_replacement(coins, 3)):
-    #     if sum(combination) <= target: 
-    #         all_combs.append(combination)
-    #         print(list(all_combs))
-        
+    def _combs(i, combination, sum_combination):
+        if sum_combination == target:
+            valid_list.append(combination)
+        else: 
+            for j in range(i, len(coins)):
+                if sum_combination <= target:
+                    _combs(j, combination + [coins[j]], sum_combination + coins[j])
+                    
+    _combs(0, [], 0)
+    return valid_list
+
 combinations(coins, 3)
+
+print(combinations(coins, k))
+
+def find_fewest_coins(combinations):
+    smallest_comb = combinations[0]
+    for combination in combinations:
+        #compare combination to previous smallest combination
+        if len(combination) < len(smallest_comb):
+            smallest_comb = combination
+    return smallest_comb
+    
+print(find_fewest_coins(combinations(coins, k)))
