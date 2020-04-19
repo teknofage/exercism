70b8f4c0f5e7722190d2f5ce606269871a54065e
teknofage <teknofage@gmail.com>
2020-04-18 22:08:09 -0700

    combinations started

---
+++ python/change/change.py
@@ -0,0 +1,21 @@
+
+coins = [1, 4, 5]
+target = 12
+def find_fewest_coins(coins, target):
+    pass
+
+
+def combinations(coins, k):
+    all_combs = []
+    # for coin in coins: 
+    #     n choose k
+    
+    
+    
+    # print (list(combinations_with_replacement (coins, 3)))
+    # for combination in list(combinations_with_replacement(coins, 3)):
+    #     if sum(combination) <= target: 
+    #         all_combs.append(combination)
+    #         print(list(all_combs))
+        
+combinations(coins, 3)
